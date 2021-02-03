6fdce33a0ecb4a1b9f62ce1121c608e5872af173
nils.cc <mail@nils.cc>
2021-02-03 20:46:59 +0100

    Modularize okay?

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
-import irc.bot
-import irc.client
-
+# imports for logging
 import argparse
 import jaraco.logging
 
+# imports for SSL support
 import ssl
+import irc.connection
 
-NICKNAME='weakbets'
-REALNAME=NICKNAME
+# the main bot
+import weakbets.bot
 
-SRV_FREENODE = irc.bot.ServerSpec('irc.freenode.net', port=7070)
-CHANNEL = '##weakpots'
-
-class WeakBets(irc.bot.SingleServerIRCBot):
-    def __init__(self, nickname=NICKNAME, realname=REALNAME, server_list=[SRV_FREENODE], channel=CHANNEL, **kwargs):
-        super().__init__(server_list=server_list, nickname=nickname, realname=realname, **kwargs)
-        self.channel = channel
-
-    def on_nicknameinuse(self, c, e):
-        c.nick(c.get_nickname() + '_')
-
-    def on_welcome(self, c, e):
-        c.join(self.channel)
-
-    def on_privmsg(self, c, e):
-        print('PRIVMSG', c, e)
-
-    def on_pubmsg(self, c, e):
-        print('PUBMSG', c, e)
 
 def main():
 
@@ -41,8 +22,9 @@ def main():
     jaraco.logging.setup(args)
 
     # start the bot
-    bot = WeakBets(connect_factory=ssl_factory)
+    bot = weakbets.bot.WeakBets(connect_factory=ssl_factory)
     bot.start()
+
 
 if __name__ == '__main__':
     main()
