e0d259df63d19cfd439be4c55ec5b4c53ff9dd83
nils.cc <mail@nils.cc>
2021-02-03 21:33:19 +0100

    Example argparse command

--- weakbets/modules.py
+++ weakbets/modules.py
@@ -1,8 +1,30 @@
-def handle(bot, e, cmd):
-    print('Handle command:', cmd)
+import argparse
 
-    nck = e.source.nick
-    chn = e.target
+def handle(bot, e, cmd):
+    print('Handle command:', cmd, e)
+
+    base = cmd.split()[0]
+    args = cmd.split()[1:]
+    print(base, args)
+
+    if base == 'test':
+        do_test(bot, e, args)
+    else:
+        bot.connection.privmsg(e.target, f'No such module: {base}')
+
+def do_test(bot, e, cmd):
     con = bot.connection
 
-    con.privmsg(chn, f'Hello {nck}!')
+    parser = argparse.ArgumentParser(
+            prog=con.get_nickname(),
+            add_help=False,
+            exit_on_error=False,
+        )
+
+    parser.add_argument('numbers', type=int, nargs='?')
+
+    try:
+        args = parser.parse_args(cmd)
+        con.privmsg(e.target, f'ok: {args.numbers}')
+    except argparse.ArgumentError as err:
+        con.privmsg(e.target, f'failed: {err}')
